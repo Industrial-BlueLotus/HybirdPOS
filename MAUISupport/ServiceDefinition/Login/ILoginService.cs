6393b2ef1d3569e3dd0be62666b5c76c648cffb5
dhvihan <danulvihan@gmail.com>
2023-01-25 15:50:57 +0530

    login imports

---
+++ MAUISupport/ServiceDefinition/Login/ILoginService.cs
@@ -0,0 +1,13 @@
+﻿using MAUISupport.ViewModels;
+using MAUISupport.Shared;
+
+
+namespace MAUISupport.ServiceDefinition.Login
+{
+    public interface ILoginService
+    {
+        Task<TokenResponse> Login(TokenRequest tok);
+        Task<IResult> Logout();
+        //Task<CompletedUserAuth> GetUserInformation();
+    }
+}
---
+++ MAUISupport/Shared/IResult.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace MAUISupport.Shared
+{
+    public interface IResult
+    {
+        List<string> Messages { get; set; }
+
+        bool Succeeded { get; set; }
+    }
+
+    public interface IResult<out T> : IResult
+    {
+        T Data { get; }
+    }
+}
+
+
